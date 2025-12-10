d0787eca7f56c93ef427806c41634a950907dfcc
malakyesheridan <malakye@easyflow.au>
2025-12-10 17:31:41 +0800

    Fix TradingView iframe X-Frame-Options error - Replace /chart/ URL with widget embed (chart/ cannot be embedded) - Enable full features: side toolbar, symbol edit, date ranges, details, calendar, hotlist, news - Widget embed is the only embeddable TradingView option

--- src/components/signals/TradingViewWrapper.tsx
+++ src/components/signals/TradingViewWrapper.tsx
@@ -4,7 +4,7 @@ export default function TradingViewWrapper() {
   return (
     <div className="w-full mt-6">
       <iframe 
-        src="https://www.tradingview.com/chart/" 
+        src="https://www.tradingview.com/widgetembed/?symbol=BINANCE:BTCUSDT&interval=D&hidesidetoolbar=0&symboledit=1&saveimage=0&toolbarbg=1e1e1e&studies=&theme=dark&style=1&timezone=Etc/UTC&locale=en&withdateranges=1&range=1M&allow_symbol_change=1&details=1&calendar=1&hotlist=1&news=1" 
         width="100%" 
         height="800" 
         style={{ border: 0, borderRadius: "12px" }} 
