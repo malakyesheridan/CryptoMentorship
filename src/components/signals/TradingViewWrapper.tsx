dad7263e4205eba6f1fb765edc0f1e5acaaaa52f
malakyesheridan <malakye@easyflow.au>
2026-01-09 22:43:13 +0800

    Update TradingViewWrapper component

--- src/components/signals/TradingViewWrapper.tsx
+++ src/components/signals/TradingViewWrapper.tsx
@@ -45,7 +45,7 @@ export default function TradingViewWrapper() {
       <iframe
         id="tv-frame"
         ref={iframeRef}
-        src="https://www.tradingview.com/widgetembed/?symbol=BINANCE:BTCUSDT&interval=D&hidesidetoolbar=0&symboledit=1&saveimage=0&toolbarbg=1e1e1e&studies=&theme=dark&style=1&timezone=Etc/UTC&locale=en&withdateranges=1&range=1M&allow_symbol_change=1&details=1&calendar=1&hotlist=1&news=1"
+        src="https://www.tradingview.com/widgetembed/?symbol=BTCUSD&interval=D&hidesidetoolbar=0&symboledit=1&saveimage=0&toolbarbg=1e1e1e&studies=&theme=dark&style=1&timezone=Etc/UTC&locale=en&withdateranges=1&range=1M&allow_symbol_change=1&details=1&calendar=1&hotlist=1&news=1"
         width="100%"
         height="800"
         style={{ border: 0, borderRadius: "12px" }}
